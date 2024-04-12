fb89843583d359a0b6f192dc707642798215d670
PAVUNRAJ <pavunrajtech@gmail.com>
2024-04-12 23:48:58 +0530

    Initial commit

---
+++ Headers/APIHandler.h
@@ -0,0 +1,18 @@
+//
+//  APIHandler.h
+//  APIHandler
+//
+//  Created by PavunRaj on 12/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for APIHandler.
+FOUNDATION_EXPORT double APIHandlerVersionNumber;
+
+//! Project version string for APIHandler.
+FOUNDATION_EXPORT const unsigned char APIHandlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIHandler/PublicHeader.h>
+
+
