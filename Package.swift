7fafd828adb49403d02c48c46658d3ffcb1887bb
Pavunraj-Palanisamy <pavunrajtech@gmail.com>
2024-04-13 09:00:51 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,3 +1,32 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "APIHandler",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_15),
+        .watchOS(.v6),
+        .tvOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "APIHandler",
+            targets: ["APIHandler"]
+        )
+    ],
+    dependencies: [
+        // Add dependencies here
+    ],
+    targets: [
+        .target(
+            name: "APIHandler",
+            dependencies: [],
+            path: "APIHandler"
+        )
+    ]
+)
+
 
 
 
